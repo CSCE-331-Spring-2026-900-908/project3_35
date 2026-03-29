aba6ef4e1db424bff9884afd9411ffa5a88c5265
Philip Tran <144759575+Toonyie@users.noreply.github.com>
2026-03-28 20:20:10 -0500

    added a .gitignore to ignore the environment file that will be used to connect to the database + added server.js which will be our server file using node.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const PORT = 3000; 
 app.use(express.static('public'));
 
+// ---- GET REQUESTS --------
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/portal_page.html'));
 });
 
-// Get requests for views
 app.get('/customer', (req, res) => {
     res.send('<h1>Customer Kiosk MVP</h1><p>Menu loading from DB soon...</p>');
 });
@@ -31,4 +31,57 @@ app.get('/kitchen', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+// ---- DATABASE CONNECTION ---------
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { rejectUnauthorized: false } 
+});
+
+// Update your Customer route to show REAL data
+app.get('/api/menu', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT name, price_per_unit, menu_item_category FROM menu_item ORDER BY menu_item_category');
+        res.json(result.rows); // Sends the database rows as a JSON array
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Database query failed" });
+    }
+});
+
+// Testing on customer page with peak sales
+app.get('/customer', async (req, res) => {
+    try {
+        const sql = `
+            SELECT o.order_time::date AS day, SUM(o.price)::numeric(12,2) AS total_sales 
+            FROM orders o 
+            GROUP BY o.order_time::date 
+            ORDER BY total_sales DESC 
+            LIMIT 10
+        `;
+        
+        const result = await pool.query(sql);
+        
+        let html = "<h1>MVP: Peak Sales Days (Live from DB)</h1><ul>";
+        result.rows.forEach(row => {
+            html += `<li>Date: ${row.day.toDateString()} | Sales: $${row.total_sales}</li>`;
+        });
+        html += "</ul><a href='/'>Back to Portal</a>";
+        
+        res.send(html);
+    } 
+    
+    catch (err) {
+        console.error(err);
+        res.status(500).send("Database Error: Check server logs.");
+    }
 });
