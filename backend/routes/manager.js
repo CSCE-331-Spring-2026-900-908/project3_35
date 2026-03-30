651abc13d4d9ab01b177030bd1ab6d2686bdc712
dongvo <dongvo@tamu.edu>
2026-03-29 20:52:02 -0500

    deleted junk and made temp the main folder

--- backend/routes/manager.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Skeleton code
-router.get('/', (req, res) => {
-    res.send('<h1>Manager Dashboard</h1><p>Status: Connected to Route Specialist</p>');
-});
-
-module.exports = router;
